e0feb4f4648d50c403e466486e84dd2a13296c4a
Zvonimir Relja <zvonimir@brain-it.hr>
2020-05-11 17:41:26 +0200

    add workbox-cli package

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="logo" />
+        <img src={logo} style={{maxWidth: 350}} alt="logo" />
         <p>
          Example of offline PWA app.
         </p>
